0468d85644fc214497fa1fbceb12e7a38e554d28
akanksha eppakayala <116954775+akanksha2345@users.noreply.github.com>
2023-05-21 15:03:53 +0530

    Add files via upload

---
+++ ifandloop.js
@@ -0,0 +1,15 @@
+
+    var x=20;
+    console.log(x+"<br>");
+
+    for (i=0;i<=20;i++){
+        console.log(i);
+    }
+    
+
+if(x=20){
+    console.log("true");
+
+}else{
+    console.log("false");  
+}
